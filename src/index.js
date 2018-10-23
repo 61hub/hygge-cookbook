e9f1702bd5518216943f28d0f3a4ffc2124aaaae
Kate <katerina_ti@mail.ru>
2018-10-23 19:07:51 +0300

    added static data and render it

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom";
 import Recipe from "./Recipe";
 
+import { recipe } from "./staticData"
+
+const mapedRecipe = recipe.map(
+  (recipe) => <Recipe title={recipe.title} description={recipe.description} />
+);
+
 const container = document.querySelector("#container");
 
 ReactDOM.render(
-  <Recipe />,
+  mapedRecipe,
   container
 );
