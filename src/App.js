16fe428bb8782e895616a730bbc4c41f57e1f52e
Kate <katerina_ti@mail.ru>
2018-11-08 16:12:30 +0300

    added styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, {Component} from "react"
 import Recipe from "./Recipe";
 import RecipeForm from "./RecipeForm"
+import "./index.css"
 
 class App extends Component {
   constructor (props) {
     super(props);
+    let recipeData = JSON.parse(localStorage.getItem("formData"));
+    recipeData = (recipeData == null) ? [] : recipeData;
+
     this.state = {
-      recipeData: JSON.parse(localStorage.getItem("formData"))
+      recipeData: recipeData
     }
 
   }
   render () {
     const saveToLs = (formData) => {
-      localStorage.setItem("formData", JSON.stringify(formData));
-      this.setState({recipeData: formData});
+      const newRecipeData = this.state.recipeData;
+      console.log(newRecipeData);
+      newRecipeData.push(formData);
+      this.setState({recipeData: newRecipeData});
+      localStorage.setItem("formData", JSON.stringify(newRecipeData));
     };
 
-    return(
+    return (
       <div>
-        <Recipe recipeData={this.state.recipeData} />
+        {this.state.recipeData.map((recipe) => <Recipe recipeData={recipe}/>)}
         <RecipeForm saveToLs={saveToLs} />
       </div>
     )
--- src/Recipe.js
+++ src/Recipe.js
@@ -4,7 +4,7 @@ import React, { Component } from "react"
 class Recipe extends Component {
   render(){
     return(
-    <div >
+    <div className="recipe">
       {this.props.recipeData == null ? "" : <div><h1>{this.props.recipeData.title}</h1> <p>{this.props.recipeData.description}</p></div>}
       </div>
     )
